1687a0c711032fcd821f028a6752129887c86686
Ubaid <urussell0802@gmail.com>
2022-06-24 21:54:05 -0400

    +getIdealSleepHours()
    Created a new function to get ideal sleep hours.

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
 function getSleepHours(day){
   switch (day){
     case 'sunday':
-    return 8
+    return 7
     break;
     case 'monday':
-    return 8
+    return 7
     break;
     case 'tuesday':
-    return 8
+    return 5
     break;
     case 'wednesday':
     return 8
     break;
     case 'thursday':
-    return 8
+    return 7
     break;
     case 'friday':
-    return 8
+    return 6
     break;
     case 'saturday':
-    return 8
+    return 7
     break;
   }
 }
+
+const getIdealSleepHours = () =>{
+  const idealHours = 8;
+  return idealHours * 7;
+}
+
 
 const getActualSleepHours = () => getSleepHours('sunday') + getSleepHours('monday') + getSleepHours('tuesday') + getSleepHours('wednesday') + getSleepHours('thursday') + getSleepHours('friday') + getSleepHours('saturday');
 
 console.log(getActualSleepHours());
 console.log(getSleepHours('monday'));
+console.log(getIdealSleepHours);
